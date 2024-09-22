b7f9c8927231f040e66329c385a94cb6c7d23c17
kush sharma <kush.73888@gmail.com>
2024-09-22 12:33:02 +0530

    Other way to do it

--- Arrays/Two-Sum-Problem.js
+++ Arrays/Two-Sum-Problem.js
@@ -1,7 +1,7 @@
 // Problem Statement:
-// You are given an array of integers and a target sum. You need to find two numbers in the array that add up to the target sum and return their indexes. There’s always one solution, and you cannot use the same element twice.
+// You are given an array of integers and a target sum. You need to find two numbers in the array that add up to the target sum and return their indexes. There's always one solution, and you cannot use the same element twice.
 
-// we can solve this more efficiently using a hash map (object in JavaScript).
+// We can solve this efficiently using a hash map (object in JavaScript).
 
 const twoSum = (arr, target) => {
     let map = {}; // empty object to store index of elements
@@ -14,9 +14,29 @@ const twoSum = (arr, target) => {
             return [map[complement], i]; // Return indices if we found the pair
         } 
 
-         // Otherwise, store the current number with its index
+        // Otherwise, store the current number with its index
         map[arr[i]] = i;
     }
+    
+    return null; // Return null if no solution is found (though problem states there's always one solution)
 }
 
 console.log(twoSum([2, 7, 11, 15], 9));
+
+// Time Complexity: O(n) where n is the length of the array
+// Space Complexity: O(n) in the worst case, we might need to store all elements in the hash map
+
+// Another way to do it using Hash map
+
+const twoSumWithHash = (arr, target) => {
+    const numToIndexMap = {};
+
+    for(let i = 0; i < arr.length; i++) {
+        const complement = target - arr[i];
+        if(complement in numToIndexMap) {
+            return [numToIndexMap[complement], i];
+        }
+        numToIndexMap[arr[i]] = i;
+    }
+    return null;
+}
